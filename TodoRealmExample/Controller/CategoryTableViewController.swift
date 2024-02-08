dc15986ab342b172443a2a29ffe6635726bb4ca2
aSquare <artur.anissimov@gmail.com>
2024-02-08 22:36:11 +0200

    Play with design and some bug fix

--- TodoRealmExample/Controller/CategoryTableViewController.swift
+++ TodoRealmExample/Controller/CategoryTableViewController.swift
@@ -14,12 +14,38 @@ class CategoryTableViewController: UITableViewController {
     
     var categories: Results<Category>?
 
+    @IBOutlet var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
 //        print(Realm.Configuration.defaultConfiguration.fileURL)
         
         load()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        let randomColor: UIColor = .random
+        
+        guard let navBar = navigationController?.navigationBar else {
+            fatalError("Navigation controller does not exist")
+        }
+        
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = randomColor
+        appearance.shadowColor = .clear
+        navBar.standardAppearance = appearance;
+        navBar.scrollEdgeAppearance = navBar.standardAppearance
+        navBar.tintColor = .black
+        
+        searchBar.barTintColor = randomColor
+        searchBar.tintColor = randomColor
+        searchBar.searchTextField.backgroundColor = .white
+        searchBar.layer.borderWidth = 1
+        searchBar.layer.borderColor = randomColor.cgColor
+        
     }
 
     // MARK: - Table view data source
@@ -33,6 +59,8 @@ class CategoryTableViewController: UITableViewController {
         var content = cell.defaultContentConfiguration()
         
         content.text = categories?[indexPath.row].name ?? "No Categories added yet"
+        
+//      cell.backgroundColor = UIColor(hexString: categories[indexPath.row].color)
         
         cell.contentConfiguration = content
 
@@ -112,6 +140,7 @@ class CategoryTableViewController: UITableViewController {
         present(alert, animated: true)
     }
     
+    // MARK: - Manipulation Methods
     func save(category: Category) {
         do {
             try realm.write {
@@ -131,6 +160,7 @@ class CategoryTableViewController: UITableViewController {
     
 }
 
+// MARK: - SearchBar Delegate Methods
 extension CategoryTableViewController: UISearchBarDelegate {
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
@@ -151,6 +181,64 @@ extension CategoryTableViewController: UISearchBarDelegate {
             
             DispatchQueue.main.async {
                 searchBar.resignFirstResponder()
+            }
+        }
+    }
+}
+
+// MARK: - UIColor Extensions
+extension UIColor {
+
+    // Get Random color
+    static var random: UIColor {
+        return UIColor(
+            red: .random(in: 0 ... 1),
+            green: .random(in: 0 ... 1),
+            blue: .random(in: 0 ... 1),
+            alpha: 1)
+    }
+    
+    // Create from HEX String the UIColor object
+    convenience init(hexString: String) {
+            let hex = hexString.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+            var int = UInt64()
+            Scanner(string: hex).scanHexInt64(&int)
+            let a, r, g, b: UInt64
+            switch hex.count {
+            case 3: // RGB (12-bit)
+                (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+            case 6: // RGB (24-bit)
+                (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+            case 8: // ARGB (32-bit)
+                (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+            default:
+                (a, r, g, b) = (255, 0, 0, 0)
+            }
+            self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: CGFloat(a) / 255)
+        }
+    
+    static func color(withCodedString string: String) -> UIColor?{
+         guard let data = Data(base64Encoded: string) else{
+             return nil
+         }
+
+         return try? NSKeyedUnarchiver.unarchivedObject(ofClass: UIColor.self, from: data)
+
+     }
+}
+
+
+// MARK: - UISearchBar Extension
+extension UISearchBar {
+    func setTextFieldColor(_ color: UIColor) {
+        for subView in self.subviews {
+            for subSubView in subView.subviews {
+                let view = subSubView as? UITextInputTraits
+                if view != nil {
+                    let textField = view as? UITextField
+                    textField?.backgroundColor = color
+                    break
+                }
             }
         }
     }
